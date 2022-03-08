c6c6ded9a16ea8affcb85e19ce98380d227c8da9
Arieleketely <arieleketely2012@hotmail.com>
2022-03-08 10:40:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+  );
+}
+
+export default App;
